6dd42a210f5b4a9fcc915a1ed2bc43cff9e3dd8d
Marco André Mendes <marcoandre@gmail.com>
2024-03-25 11:42:47 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+import router from './router';
+<template>
+  <header>
+    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link to="/categorias">Categorias</router-link> |
+    <router-link to="/editoras">Editoras</router-link> |
+    <router-link to="/autores">Autores</router-link>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
